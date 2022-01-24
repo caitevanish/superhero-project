627417ef846e4fc48bb886a64483a8657431c245
caitevanish <cait.evan@gmail.com>
2022-01-23 16:33:39 -0800

    Implemented A LOT

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -6,6 +6,7 @@ app_name = 'superheroes'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:hero_id>', views.details, name='details'),
-  path('new/', views.create, name='create')
+  path('new/', views.create, name='create'),
+  path('<int:hero_id>/update', views.update, name='update')
   
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -28,10 +28,46 @@ def create(request):
     primary = request.POST.get('primary')        
     secondary = request.POST.get('secondary')
     catchphrase = request.POST.get('catchphrase')
+   
     new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
     new_hero.save()
     return HttpResponseRedirect(reverse('superheroes:index'))
 
   else:
     return render(request, 'superheroes/create.html')
-  
+
+
+def update(request, hero_id):
+  update_hero = Superhero.objects.get(pk=hero_id)
+  context = {
+      'hero' : update_hero
+  }
+
+  if request.method == "POST": #?
+    name = request.POST.get('name')
+    alter_ego = request.POST.get('alter_ego')
+    primary = request.POST.get('primary')        
+    secondary = request.POST.get('secondary')
+    catchphrase = request.POST.get('catchphrase')
+    
+    update_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+    update_hero.save()
+    
+    return HttpResponseRedirect(reverse('superheroes:index'))
+
+  else:
+    return render(request, 'superheroes/update.html', context)
+    
+
+
+def delete(request, hero_id):
+  single_hero = Superhero.objects.get(pk=hero_id)
+  context = {
+    'hero': single_hero
+  }
+
+  if request.method == 'POST':
+    pass
+
+    return HttpResponseRedirect(reverse('superheroes:index'))
+  return render(request, 'superheroes/details.html', context)
